cca8e6c20afcaff365f4073b424d879e6ef3e754
suweiquan <1096256362@qq,com>
2018-09-03 22:16:20 +0800

    Initial commit

---
+++ src/com/suweiquan/main/WaitAndNotifyThread.java
@@ -0,0 +1,38 @@
+package com.suweiquan.main;
+
+public class WaitAndNotifyThread {
+    private static boolean isWait = true;
+    private static Object o = new Object();
+    public static void main(String[] args) throws Exception {
+        Thread waitThread = new Thread(new WaitAndNotifyThread.WaitThread());
+        waitThread.start();
+        Thread.sleep(1000);
+        Thread notifyThread = new Thread(new WaitAndNotifyThread.NotifyThread());
+        notifyThread.start();
+        System.out.println("hello world");
+    }
+
+    static class WaitThread implements Runnable{
+        @Override
+        public void run() {
+            synchronized (o){
+                while(isWait){
+                    try {
+                        o.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("wait finished");
+            }
+            System.out.println("hello world");
+        }
+    }
+
+    static class NotifyThread implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }
+}
