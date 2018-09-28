7ebf42e7073fc108c76e2537ed35d18857864af9
suweiquan <weiquan@imagedt.com>
2018-09-28 19:40:24 +0800

    回滚提交

---
+++ src/com/suweiquan/main/Application.java
@@ -0,0 +1,12 @@
+package com.suweiquan.main;
+
+/**
+ * User: suweiquan
+ * Date: 2018-09-04
+ */
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("this is a master-bak-bak branch");
+    }
+}
